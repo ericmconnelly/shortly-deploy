04ee477013648dd210abc83cc5c859125080fd65
Hack Reactor Students <accounts@hackreactor.com>
2015-07-30 18:57:04 -0700

    pushing code

--- app/config.js
+++ app/config.js
@@ -1,50 +1,14 @@
-// var Bookshelf = require('bookshelf');
-// var path = require('path');
+// exports.mongoose.connect('mongodb://MongoLab-r:f3BWB.ORBBLTrl..oUe5h4.NZvcet_bwG7lk5hmbRjI-@ds036178.mongolab.com:36178/MongoLab-r/shortlydb');
 
-// ///////////////////////////////
-// //MONGOOSE
-// // Retrieve
-// exports.mongoose = require('mongoose');
-// var Schema = exports.mongoose.Schema;
-// // var ObjectId = Schema.ObjectId;
-// exports.mongoose.connect('mongodb://localhost/shortlydb');
-// // exports.mongoose.connect('mongodb://MongoLab-r:f3BWB.ORBBLTrl..oUe5h4.NZvcet_bwG7lk5hmbRjI-@ds036178.mongolab.com:36178/MongoLab-r/shortlydb');
-
-// var db = exports.mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'connection error:'));
-
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function(callback) {
-//   console.log('Connected to mongo database');
-// });
-
-// db.urlSchema = mongoose.Schema({
-//   url: String,
-//   base_url: String,
-//   code: String,
-//   title: String,
-//   visits: {type: Number, default: 0},
-//   timestamp: {type: Date, default: Date.now}
-// });
-
-// db.userSchema = mongoose.Schema({
-//   username: {type: String, unique: true, required: true},
-//   password: {type: String, maxlength: 100},
-//   timestamp: {type: Date, default: Date.now}
-// });
-
-
-// module.exports = db;
 
 var mongoose = require('mongoose');
 // var path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
   console.log("USING PROD DB");
-  mongoose.connect('mongodb://MongoLab-r:f3BWB.ORBBLTrl..oUe5h4.NZvcet_bwG7lk5hmbRjI-@ds036178.mongolab.com:36178/MongoLab-r/shortlydb');
+  mongoose.connect('mongodb://kevken:shortly123@ds038888.mongolab.com:38888/shortly');
 } else {
+  console.log("USING TEST DB");
   mongoose.connect('mongodb://localhost/test');
 }
 
@@ -71,67 +35,3 @@ db.userSchema = mongoose.Schema({
 
 
 module.exports = db;
-
-
-// exports.urlsModel = mongoose.model('Link', exports.urlSchema, 'urls');
-// exports.usersModel = mongoose.model('User', exports.userSchema, 'users');
-
-// exports.connection = mongoose.connection;
-
-///////////////////////////////
-
-
-// mongoose.connection.db.listCollections({name: 'mycollectionname'})
-//     .next(function(err, collinfo) {
-//         if (collinfo) {
-//             // The collection exists
-//         }
-//     });
-
-
-
-
-
-
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
--- app/models/user.js
+++ app/models/user.js
@@ -3,11 +3,7 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-
-
 db.userSchema.pre('save', function(next) {
-
-  // hash password
   var cipher = Promise.promisify(bcrypt.hash);
   return cipher(this.password, null, null).bind(this)
     .then(function(hash) {
@@ -22,33 +18,6 @@ db.userSchema.methods.comparePassword = function(attemptedPassword, callback) {
     });
   };
 
-// db.userSchema.methods.hashPassword = function(){
- 
-//   };
-
-
-
-
 var User = mongoose.model('User', db.userSchema);
-
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
 
 module.exports = User;
